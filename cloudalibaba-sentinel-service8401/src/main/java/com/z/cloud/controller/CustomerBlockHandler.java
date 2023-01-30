8bbfc08ebecf6c94278fda2767a31d30aa6e6b87
z <306979910@qq.com>
2023-01-30 14:13:14 +0800

    z

--- cloudalibaba-sentinel-service8401/src/main/java/com/z/cloud/controller/CustomerBlockHandler.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/z/cloud/controller/CustomerBlockHandler.java
@@ -11,7 +11,7 @@ public class CustomerBlockHandler
 {
     public static CommonResult handlerException(BlockException exception)
     {
-        return new CommonResult(4444,"按客戶自定义,global handlerException----1");
+        return new CommonResult(4444,"按客戶自定义1,global handlerException----1");
     }
     public static CommonResult handlerException2(BlockException exception)
     {
